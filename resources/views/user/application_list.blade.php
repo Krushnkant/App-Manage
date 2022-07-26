3acf1365ecffe4f82978b0c693f2d0cc03e96107
poojawebvedant <poojawebvedant@gmail.com>
2022-07-26 10:25:01 +0530

    working on first test

--- resources/views/user/application_list.blade.php
+++ resources/views/user/application_list.blade.php
@@ -90,7 +90,6 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script> -->
 <script type="text/javascript">
-    // console.log("{{ url('application') }}");
     $(function () {
         var table = $('.data-table').DataTable({
             processing: true,
