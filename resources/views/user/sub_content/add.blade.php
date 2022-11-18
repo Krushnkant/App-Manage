1a0aab003e6ccbd740e8c565ee791da53ab6097a
Zahid Shaikh <zahid.shaikh.9013@gmail.com>
2022-11-18 16:27:28 +0530

    working 18-11-2022 4

--- resources/views/user/sub_content/add.blade.php
+++ resources/views/user/sub_content/add.blade.php
@@ -51,7 +51,7 @@
       <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="{{url('dashboard')}}">Dashboard</a></li>
         <li class="breadcrumb-item "><a href="{{url('application-new')}}">Application List</a></li>
-        <li class="breadcrumb-item "><a href="{{url('application-new-design/'.$app_id.'/'.$cat_id.'/'.$parent_id)}}">Back List</a></li>
+        <li class="breadcrumb-item "><a href="{{url('application-new-design/'.$cat_id.'/'.$app_id.'/'.$parent_id)}}">Back List</a></li>
         <li class="breadcrumb-item active">Add Content</li>
       </ol>
     </div>
